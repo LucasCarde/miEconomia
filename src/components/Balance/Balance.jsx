bb771884ccebfae4bf6611a3fc84eebd5d125158
Carde <70904902+LucasCarde@users.noreply.github.com>
2024-12-29 13:29:13 -0300

    visibilidad balance

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
+import {useState} from 'react'
 import './balance.css'
-import { IoEyeOutline } from "react-icons/io5";
+import {IoEyeOutline} from "react-icons/io5";
 
 const Balance = () => {
-  return (
-    <>
-    <div className="balance-card">
-        <div className="cardTitle">Balance</div>
-        <div className="amount">$ 576.034,15</div>
-        <IoEyeOutline className="icon"></IoEyeOutline>
-    </div>
-    </>
+    const [show, setShow] = useState(true)
 
-  )
+    const changeVisibility = () => {
+        setShow(!show)
+    }
+    return (
+        <> < div className = "balance-card" > <div className="cardTitle">Balance</div>
+            {
+            show
+                ? <div className="amount">$ 576.034,15</div>
+                : <div className="amount">$ *********</div>
+        } < IoEyeOutline className = "icon" onClick = {
+            changeVisibility
+        } > </IoEyeOutline>
+</div>
+</>
+
+    )
 }
 
 export default Balance
--- src/components/UltimosMovimientos/UltimosMovimientos.jsx
+++ src/components/UltimosMovimientos/UltimosMovimientos.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Balance from '../Balance/Balance'
 import Pagetitle from '../PageTitle/PageTitle'
-import CategoriaContainer from '../CategoriaContainer/CategoriaContainer'
 
 const UltimosMovimientos = () => {
   return (
