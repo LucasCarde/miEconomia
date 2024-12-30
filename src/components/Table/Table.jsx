65b5d1a7ad5e2457eebfada32a2ae24d5ad75d3c
Carde <70904902+LucasCarde@users.noreply.github.com>
2024-12-30 16:09:06 -0300

    cambio estetico menor

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import TableHeader from './TableHeader/TableHeader'
 import TableBody from './TableBody/TableBody'
+import './Table.css'
 
 const Table = () => {
   return (
-    <>
+    <div className="table">
         <TableHeader />
         <TableBody />
-    </>
+    </div>
   )
 }
 
