092471de633534100e8865f1b2bf50d93ee5cfc7
Carde <70904902+LucasCarde@users.noreply.github.com>
2024-12-30 16:11:31 -0300

    +cambios esteticos

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/ultimosMovimientos" element={<UltimosMovimientos/>} />
+        <Route path="/agregarMovimiento" element={''} />
       </Routes>
       <Footer />
     </BrowserRouter>
