3051c088bb67f2fcab84914ebe034a0298135aa2
Carde <70904902+LucasCarde@users.noreply.github.com>
2024-12-31 12:22:53 -0300

    agrego botones en AgregarMovimientos

--- src/components/AgregarMovimiento/AgregarMovimiento.jsx
+++ src/components/AgregarMovimiento/AgregarMovimiento.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Pagetitle from '../PageTitle/PageTitle'
+import Boton from '../Boton/Boton'
 
 const AgregarMovimiento = () => {
   return (
     <>
     <Pagetitle message='agregarMovimiento'/>
+    <Boton/>
     </>
   )
 }
---
+++ src/components/Boton/Boton.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Boton.css'
+
+const Boton = () => {
+  return (
+    <div className='botones-container'>
+    <button className='botones'>Gasto</button>
+    <button className='botones'>Ingreso</button>
+    <button className='botones'>Cambio de moneda</button>
+    <button className='botones'>Gasto</button>
+    </div>
+)
+}
+
+export default Boton
